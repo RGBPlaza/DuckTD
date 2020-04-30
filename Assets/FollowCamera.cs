0767ca4fe6c1c5151597e4b994edf104b26b8000
RGBPlaza <rayred14@live.com>
2020-05-01 00:26:31 +0100

    starting to implement first person element

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public Transform TransformToFollow;
+    private Vector3 displacement;
+
+    private void Start()
+    {
+        displacement = TransformToFollow.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        //transform.position = TransformToFollow.position - displacement;
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera Camera;
+
+    public float WalkSpeed = 5f;
+    public float RotateSpeed = 10f;
+    public float Gravity = 9.8f;
+    public float JumpVelocity = 10f;
+    
+    private float verticalVelocity = 0;
+    private CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        characterController.Move(transform.up * verticalVelocity * Time.deltaTime);
+        if (characterController.isGrounded)
+        {
+            if (verticalVelocity < 0)
+                verticalVelocity = 0;
+            if (Input.GetKey(KeyCode.Period))
+                characterController.Move(transform.forward * WalkSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.E))
+                characterController.Move(-transform.forward * WalkSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.U))
+                characterController.Move(transform.right * WalkSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.O))
+                characterController.Move(-transform.right * WalkSpeed * Time.deltaTime);
+            transform.Rotate(transform.up * Input.GetAxis("Mouse X") * RotateSpeed * Time.deltaTime);
+        }
+        else
+            verticalVelocity -= Gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && verticalVelocity == 0)
+            verticalVelocity = JumpVelocity;
+    }
+
+}
